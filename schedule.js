54ae45f892a6b003244f2eaf0ceac7778f371335
hatterisk3-gif <hatterisk3@gmail.com>
2026-07-05 10:18:45 +0900

    修正

--- schedule.js
+++ schedule.js
@@ -1,4 +1,4 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbw7y4G2ltoMtBtyu0fqqClXfzOloZMm4fe1bd3zk5epOAoa7glPOcwc_8vAJxIl3lBz/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzqga3_gw7fKTFdOieVZbudC36yP7_xKWiYPu4XyPIg8ahwe2y7JcB93sGyUTrHGQWV/exec";
       let map, infoWindow, loadedPolygons = {};
       let globalSchedules = [];
       let currentDept = 'すべて'; // 現在選択されている部署フィルター
